eae8ccd7519499c775d4e16074da2ff8f4f6fec1
Impactivity <minwoo2944@gmail.com>
2022-03-26 23:54:58 +0900

    3.26

---
+++ 1072.py
@@ -0,0 +1,23 @@
+import sys
+read = sys.stdin.readline
+
+x,y = map(int,read().split())
+
+l = 1
+r = 1000000000
+
+z = y * 100 // x
+answer = 0
+while l <= r:
+    mid = (r + l) // 2
+
+    if (y+mid)*100 // (x+mid)  > z:
+        r = mid -1
+        answer = mid
+    else :
+        l = mid + 1
+
+if r == 1000000000:
+    print(-1)
+else:
+    print(answer)
